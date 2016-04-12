a6cda80009ba7ef029db0a812d87ad46be2dee66
Henryk Konsek <hekonsek@gmail.com>
2016-04-12 10:03:29 +0200

    Added src.

---
+++ public/src/app/app.ts
@@ -0,0 +1,8 @@
+/// <reference path="../../../typings/main.d.ts" />
+
+angular.module('myappApp', [
+    'app.demo',
+    'app.templates'
+]);
+
+// your app setup here
---
+++ public/src/demo/demo.ts
@@ -0,0 +1,45 @@
+/// <reference path="../../../typings/main.d.ts" />
+
+module app.demo {
+
+    'use strict';
+
+    export interface IDemoCtrl {}
+    export class DemoCtrl implements IDemoCtrl {
+
+        scope: any;
+        http: ng.IHttpService;
+
+        constructor(
+            $scope: ng.IScope,
+            $http: ng.IHttpService
+        ){
+            this.scope = $scope;
+            this.http = $http;
+
+            this.scope.hello = function () {
+                $scope['foo'] = 'bar';
+            }
+        }
+
+    }
+
+    export interface IDemoService {
+        getExcited: boolean;
+    }
+    export class DemoService implements IDemoService {
+        getExcited: boolean = false;
+    }
+
+    angular
+        .module('app.demo', [])
+        .directive("demo", function(): ng.IDirective {
+            return {
+                templateUrl: 'app-templates/demo/demo.html',
+                controller:  DemoCtrl,
+                controllerAs: 'demoCtrlVM'
+            };
+        })
+        .controller("demoCtrl", DemoCtrl)
+        .factory("demoService", [() => new app.demo.DemoService()]);
+}
