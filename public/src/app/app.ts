5aa7c3c659b50f05229a8ce3a3b1ebec80e491d5
Henryk Konsek <hekonsek@gmail.com>
2016-04-15 18:36:28 +0200

    Added menu support.

--- public/src/app/app.ts
+++ public/src/app/app.ts
@@ -4,6 +4,13 @@ angular.module('myappApp', [
     'app.demo',
     'app.camera',
     'app.templates'
-]);
+]).controller("appCtrl", ['$scope', function($scope : any) {
+    $scope['tab'] = 'home';
+    $scope['home'] = function () {
+        $scope['tab'] = 'home';
+    };
+    $scope['camera'] = function () {
+        $scope['tab'] = 'camera';
+    };}]);
 
 // your app setup here
