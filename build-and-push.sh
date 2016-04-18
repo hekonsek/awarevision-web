1b08f8dd6551269b21831428e7bd7a88699d9e91
Henryk Konsek <hekonsek@gmail.com>
2016-04-18 15:00:16 +0200

    Added timestamped Docker tag.

--- build-and-push.sh
+++ build-and-push.sh
@@ -9,12 +9,12 @@ gulp build
 # Deploy Dockerized application
 docker build -t ${DOCKER_COORDINATES}:${VERSION} .
 
-
+# Tagging images
 docker tag -f ${DOCKER_COORDINATES}:${VERSION} ${DOCKER_COORDINATES}:latest
 TIMESTAMP=$(date +%Y-%m-%d)
 docker tag -f ${DOCKER_COORDINATES}:${VERSION} ${DOCKER_COORDINATES}:${TIMESTAMP}
 
-
+# Pushing images
 docker push ${DOCKER_COORDINATES}:${VERSION}
 docker push ${DOCKER_COORDINATES}:latest
 docker push ${DOCKER_COORDINATES}:${TIMESTAMP}
